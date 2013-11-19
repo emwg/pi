7bc6501ee5b03cae229d3c4c4845d36a326e51e0
emwg <tsuemwg@gmail.com>
2013-11-19 19:58:10 +0000

    put mixer.py in src folder

---
+++ src/mixer.py
@@ -0,0 +1,37 @@
+import sndobj
+import time
+
+tab = sndobj.HarmTable()
+osc1 = sndobj.Oscili(tab, 1, 5000)
+osc2 = sndobj.Oscili(tab, 1, 5000)
+mod = sndobj.Oscili(tab, 1000, 200)
+out = sndobj.SndRTIO(1, sndobj.SND_OUTPUT)
+delay = sndobj.DelayLine(1, osc2)
+mixer = sndobj.Mixer()
+
+
+mod.SetFreq(1)
+osc1.SetFreq(440, mod)
+osc2.SetFreq(800, mod)
+mixer.AddObj(osc1)
+mixer.AddObj(osc2)
+mixer.AddObj(delay)
+out.SetOutput(1, mixer)
+
+thread = sndobj.SndThread()
+thread.AddObj(mod)
+thread.AddObj(osc1)
+thread.AddObj(osc2)
+thread.AddObj(delay)
+thread.AddObj(mixer)
+thread.AddObj(out, sndobj.SNDIO_OUT)
+
+thread.ProcOn()
+for i in range(50):
+    mod.SetFreq(i)
+    time.sleep(0.1)
+for i in range(50, 1, -1):
+    mod.SetFreq(i)
+    time.sleep(0.1)
+time.sleep(1)
+thread.ProcOff()

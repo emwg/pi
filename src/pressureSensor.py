0633f7e932a64d41203da62b376e02e6d58a5fed
emwg <tsuemwg@gmail.com>
2013-12-03 13:39:51 -0600

    Update and rename pressuresensor.py to pressureSensor.py

--- src/pressureSensor.py
+++ src/pressureSensor.py
@@ -4,14 +4,15 @@ import sensor
 class pressureSensor:
 	
 	def __init__(self, channel, sensitivity):
-		self.sensor = sensor(self.channel,"pressure")
+		self.sensor = sensor(channel,"pressure")
 		self.sensitivity = sensitivity
 
 	def __str__(self):
+		return sensor.__str__()
 
 	def getPressureValue(self):
 		self.sensor.update()
-		if (self.sensor.getValue() > sensitivity)
+		if (self.sensor.getValue() > self.sensitivity)
 			return (self.sensor.get(Value() - sensitivity)
 		return 0
 	

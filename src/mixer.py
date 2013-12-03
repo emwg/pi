a1e2269850d4f5645e48d2a581546ccca236fb25
emwg <tsuemwg@gmail.com>
2013-12-03 13:59:41 -0600

    Update mixer.py

--- src/mixer.py
+++ src/mixer.py
@@ -49,8 +49,7 @@ while True:
     light = lsensor.getLightValue()
     print("Light: " + str(light))
     freqStep = 3
-    print(osc1)
-    if (osc1.fr < light * 2): osc1.SetFreq(osc1.fr + freqStep)
+    if (osc1.freq() < light * 2): osc1.SetFreq(osc1.fr + freqStep)
     elif (osc1.fr > light * 2): osc1.SetFreq(osc1.fr - freqStep)
     
     pressure = psensor.getPressureValue()

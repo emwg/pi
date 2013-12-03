c43076ae722da8b0f938d482484cad6e4a28ce77
emwg <tsuemwg@gmail.com>
2013-12-03 13:14:10 -0600

    Update lightsensor.py
    fixed things

--- src/lightsensor.py
+++ src/lightsensor.py
@@ -1,13 +1,13 @@
 import sensor
 
 class lightSensor:
-	
 	def __init__(self, channel, sensitivity):
 		self.sensor = sensor(self.channel,"light")
 		self.sensitivity = sensitivity
 
 	def __str__(self):
+		return sensor.__str__()
 
-	def getLightValue(self)
+	def getLightValue(self):
 		self.sensor.update()
 		return (self.sensor.get(Value() - sensitivity)

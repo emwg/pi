5a2e972881f136df582ec1112d0aacbd6c1b3c0e
emwg <tsuemwg@gmail.com>
2013-12-03 13:43:08 -0600

    Update flexSensor.py
    connsistency issues

--- src/flexSensor.py
+++ src/flexSensor.py
@@ -1,14 +1,15 @@
-import sensor
+from sensor import *
 
 class flexSensor:
 	
 	def __init__(self, channel, pluck):
-		self.sensor = sensor(self.channel,"flex")
+		self.sensor = sensor(channel,"flex")
 		self.pluck = pluck
 		self.lastValue = 0
 		self.temp = 0
 
 	def __str__(self):
+		return sensor.__str__()
 
 	def getFlexValue(self):
 		self.sensor.update()

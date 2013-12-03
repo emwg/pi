54b9d9d9453fc886976edb7bd08285138a38f370
emwg <tsuemwg@gmail.com>
2013-12-03 13:43:13 -0600

    Update pressureSensor.py

--- src/pressureSensor.py
+++ src/pressureSensor.py
@@ -12,7 +12,8 @@ class pressureSensor:
 
 	def getPressureValue(self):
 		self.sensor.update()
-		if (self.sensor.getValue() > self.sensitivity): return (self.sensor.getValue() - self.sensitivity)
+		value = self.sensor.maxValue - self.sensor.getValue()
+		if (value > self.sensitivity): return (value - self.sensitivity)
 		return 0
 	
 	

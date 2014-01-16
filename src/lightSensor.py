ff979b8d2a3bb03cc63c52e33811874266c21d57
emwg <tsuemwg@gmail.com>
2014-01-15 19:49:03 -0600

    Update lightSensor.py
    fixed error

--- src/lightSensor.py
+++ src/lightSensor.py
@@ -1,10 +1,9 @@
 from sensor import *
 
 class lightSensor:
-	def __init__(channel, sensitivity):
-		#self.sensor = sensor(channel,"light")
-		#self.sensitivity = sensitivity
-		print("Light sensor initialized")
+	def __init__(self, channel, sensitivity):
+		self.sensor = sensor(channel,"light")
+		self.sensitivity = sensitivity
 
 	def __str__(self):
 		return sensor.__str__()

1019214807117471f55686c5aefafbeea3ff7ecf
emwg <emwg@op331202.local>
2014-02-12 17:21:23 -0600

    fixed a thing

--- src/lighttest.py
+++ src/lighttest.py
@@ -70,7 +70,7 @@ osc4subtract = osc3subtract * subtractMult
 while True:
     
     #print system info
-    print(psutil.__version__())
+    print(psutil.__version__)
     print("RAM usage: " + str(psutil.virtual_memory()) + "%")
     print("CPU usage: " + str(psutil.cpu_percent(interval=0)) + "%")
     

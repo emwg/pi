2dba581018f8abad0930a0b28b5c86e1daed9ca0
emwg <tsuemwg@gmail.com>
2014-02-14 23:16:51 +0000

    Played around with harmonic tables, need to figure out how to pass in a pointer to the C function using python.

--- src/acceltest.py
+++ src/acceltest.py
@@ -3,14 +3,19 @@ import time
 import psutil
 from accelsensor import *
 
-accel = accelSensor(1, 10)
+# Create the accelerometer sensor objects
+accelY = accelSensor(0, 10)
 
-harmTable = sndobj.HarmTable()
-harmTable.SetHarm(1, sndobj.SAW)
-osc1 = sndobj.Oscili(harmTable, 0, 0)
-osc1.SetAmp(3000)
-osc1.SetFreq(1600)
+# Create the harmonic table and attach to oscilators
+# The higher the first parameter in harmTable.SetHarm is, the buzzier the sound
+coeffs = [4.0, 2.0];
+harmTable = sndobj.PlnTable(100, 2, coeffs)
+#harmTable.SetHarm(100, sndobj.SINE)
+osc1 = sndobj.Oscili(harmTable, 440, 900)
+osc1.SetAmp(9000)
+osc1.SetFreq(600)
 
+# Create a mixer
 mixer = sndobj.Mixer()
 mixer.AddObj(osc1)
 
@@ -18,10 +23,17 @@ out = sndobj.SndRTIO(1, sndobj.SND_OUTPUT)
 thread = sndobj.SndThread()
 out.SetOutput(1, mixer)
 
+# Attach sound objects to the sound thread
 thread.AddObj(osc1)
 thread.AddObj(mixer)
 thread.AddObj(out, sndobj.SNDIO_OUT)
 thread.ProcOn()
 
 while True:
-	ocs1.SetAmp(3000)
+	# Get the accelerometer values, ranging from ~(400-600), print out
+	accelYValue = accelY.getAccelValue()
+	print("AccelY:" + str(accelYValue))
+	newAmp = (accelYValue - 400)
+	if(newAmp < 0): newAmp = 0
+	osc1.SetAmp(newAmp * 20)
+	

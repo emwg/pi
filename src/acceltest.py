af58978047eadfd14134206f9cf377b688026816
emwg <emwg@op331202.local>
2014-02-25 22:21:52 -0600

    Printing Time

--- src/acceltest.py
+++ src/acceltest.py
@@ -42,10 +42,11 @@ while True:
 	accelYValue = accelY.getAccelValue()
 	print("AccelY:" + str(accelYValue))
 	print("Current Tone: " + currentTone)
+	print(time.time())
 	
 	if(time.time() > deltaT):
 		deltaT = time.time()
-		currentTone = toneLib.upSteps(2, currentTone)
+		#currentTone = toneLib.upSteps(2, currentTone)
 		osc1.SetFreq(toneLib.getToneToFreq(currentTone))
 	#newAmp = (accelYValue - 400)
 	#if(newAmp < 0): newAmp = 0

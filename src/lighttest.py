c52f51f6c87e998aee6c1c808749b6b36f0015e3
emwg <emwg@169.254.154.255>
2014-03-04 17:15:22 -0600

    sine

--- src/lighttest.py
+++ src/lighttest.py
@@ -5,7 +5,7 @@ from lightSensor import *
 
 lsensor = lightSensor(0, 10)
 
-tab = sndobj.HarmTable(1000, 20, sndobj.BUZZ)
+tab = sndobj.HarmTable(1000, 20, sndobj.SINE)
 osc1 = sndobj.Oscili(tab, 400, 5000)
 #osc2 = sndobj.Oscili(tab, 0, 0)
 #osc3 = sndobj.Oscili(tab, 0, 0)

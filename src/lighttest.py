21a046e69d764e1cd8b1de2a7a839bf9810edaac
emwg <emwg@169.254.154.255>
2014-03-04 17:20:04 -0600

    saw wave yeah

--- src/lighttest.py
+++ src/lighttest.py
@@ -5,7 +5,7 @@ from lightSensor import *
 
 lsensor = lightSensor(0, 10)
 
-tab = sndobj.HarmTable(1000, 20, sndobj.SINE)
+tab = sndobj.HarmTable(1000, 20, sndobj.SAW)
 osc1 = sndobj.Oscili(tab, 0, 0)
 #osc2 = sndobj.Oscili(tab, 0, 0)
 #osc3 = sndobj.Oscili(tab, 0, 0)

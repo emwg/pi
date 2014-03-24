89fcb239cbeb1a010d5d8dd329770b1b82ddb89c
emwg <emwg@op331202.local>
2014-03-23 19:51:23 -0500

    Testing mods

--- src/acceltest.py
+++ src/acceltest.py
@@ -30,22 +30,22 @@ noise = sndobj.Randh(10000, 1000)
 # Create a mixer
 mixer = sndobj.Mixer()
 mixer.AddObj(osc1)
-mixer.AddObj(sound2)
+#mixer.AddObj(sound2)
 
 out = sndobj.SndRTIO(1, sndobj.SND_OUTPUT)
 #out.SetOutput(1, osc1)
 
-mod = sndobj.Oscili(harmTable, 2, 50)
-osc1.SetFreq(440, mod)
+#mod = sndobj.Oscili(harmTable, 2, 50)
+#osc1.SetFreq(440, mod)
 
 thread = sndobj.SndThread()
 out.SetOutput(1, mixer)
 
 # Attach sound objects to the sound thread
 thread.AddObj(osc1)
-thread.AddObj(sound2)
+#thread.AddObj(sound2)
 thread.AddObj(mixer)
-thread.AddObj(mod)
+#thread.AddObj(mod)
 #thread.AddObj(noise)
 thread.AddObj(out, sndobj.SNDIO_OUT)
 thread.ProcOn()

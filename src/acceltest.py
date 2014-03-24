3ca73aae09c531e6053c082560f3c03c9756816a
emwg <emwg@op331202.local>
2014-03-24 15:46:48 -0500

    Tesing

--- src/acceltest.py
+++ src/acceltest.py
@@ -35,7 +35,7 @@ mixer.AddObj(osc1)
 out = sndobj.SndRTIO(1, sndobj.SND_OUTPUT)
 #out.SetOutput(1, osc1)
 
-mod = sndobj.Oscili(harmTable, 2, 50)
+mod = sndobj.Oscili(harmTable, 4, 50)
 osc1.SetFreq(440, mod)
 
 thread = sndobj.SndThread()

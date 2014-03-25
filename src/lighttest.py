d4235768d2921d4e60c556babbf1578ce2b82f05
emwg <emwg@op331202.local>
2014-03-25 16:58:47 -0500

    i don't think it works

--- src/lighttest.py
+++ src/lighttest.py
@@ -34,7 +34,7 @@ mixer.AddObj(osc3)
 mixer.AddObj(osc4)
 #mixer.AddObj(pluck1)
 #mixer.AddObj(buzz)
-lowpass = sndobj.LoPass(100, mixer)
+lowpass = sndobj.LoPass(800, mixer)
 mixer.AddObj(lowpass)
 
 pan = sndobj.Pan(0, mixer)

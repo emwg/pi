12d0d4cc77c94c17374ea5902e5df22b8c16cfb1
emwg <emwg@169.254.100.39>
2014-03-25 21:27:28 -0500

    Debug

--- src/microphoneTest.py
+++ src/microphoneTest.py
@@ -7,7 +7,7 @@ toneLib = toneLibrary()
 middleC = toneLib.getToneToFreq('C4')
 
 harmTable = sndobj.HarmTable(1024, 100, sndobj.SINE)
-osc = sndobj.Oscili(harmTable, middleC, 900)
+osc = sndobj.Oscili(harmTable, toneLib.getToneToFreq('C4'), 900)
 
 out = sndobj.SndRTIO(1, sndobj.SND_OUTPUT)
 

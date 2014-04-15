eaa0be6c382b5f9ffd3ea0eb9d929074a0976fc6
emwg <tsuemwg@gmail.com>
2014-04-15 14:40:53 -0500

    Update emwg.py

--- src/emwg.py
+++ src/emwg.py
@@ -94,7 +94,7 @@ for x in chord4:
 
 harmTable = sndobj.HarmTable()
 harmTable.SetHarm(100, sndobj.SINE)
-osc1 = sndobj.Oscili(harmTable, 440, 1000)
+osc1 = sndobj.Oscili(harmTable, 440, 1200)
 osc2 = sndobj.Oscili(harmTable, 440, 1000)
 
 # Add things to mixer
